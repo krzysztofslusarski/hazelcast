d9bb314a449846a481250858e13ecd0538146abc
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-05-26 07:59:23 +0200

    Actors

--- hazelcast/src/main/java/com/hazelcast/tpc/engine/actor/ActorHandle.java
+++ hazelcast/src/main/java/com/hazelcast/tpc/engine/actor/ActorHandle.java
@@ -2,12 +2,11 @@ package com.hazelcast.tpc.engine.actor;
 
 import com.hazelcast.tpc.engine.Eventloop;
 
+@SuppressWarnings("ALL")
 public interface ActorHandle {
-
     void send(Object message);
 
     Mailbox getMailbox();
-
 
     Eventloop getEventloop();
 }
